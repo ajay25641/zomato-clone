ea7bfab543b4ccaf5ed029bfd77e3b857e2c723d
ajay25641 <ajay25641@gmail.com>
2021-11-18 22:50:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const router = require("./Router/index");
+
+const PORT = 2109;
+const host = "localhost";
+const dbUrl =
+  "mongodb+srv://developer_ajk:ajay@12345@cluster0.c65dz.mongodb.net/zomato_DB?retryWrites=true&w=majority";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true })
+  .then((res) => {
+    app.listen(PORT, host, () => {
+      console.log(`Server is running on ${host}:${PORT} `);
+    });
+  })
+  .catch((err) => console.log(err));
